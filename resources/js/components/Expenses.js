28a89964c1fb63cf51cc67cf621778db558ff7c8
nhsykym <nhsykym@gmail.com>
2020-03-24 08:01:29 +0000

    #43 Change ブレイクポイントsm未満の時表示列を減らす

--- resources/js/components/Expenses.js
+++ resources/js/components/Expenses.js
@@ -7,8 +7,13 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Title from './Title';
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const Expenses = (props) => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('sm'));
+  
     return (
         <React.Fragment>
           <Title>最近の支出</Title>
@@ -18,13 +23,21 @@ const Expenses = (props) => {
                 <TableCell>日付</TableCell>
                 <TableCell>摘要</TableCell>
                 <TableCell>金額</TableCell>
-                <TableCell>カテゴリ</TableCell>
-                <TableCell>編集</TableCell>
+                {matches ?
+                  <React.Fragment>
+                    <TableCell>カテゴリ</TableCell>
+                    <TableCell align="center">編集</TableCell>
+                  </React.Fragment>
+                :
+                    ''
+                }
+                
+                
               </TableRow>
             </TableHead>
             <TableBody>
                 <RenderRows
-                    expenses={props.expenses} updateTable={props.updateTable}
+                    expenses={props.expenses} updateTable={props.updateTable} matches={matches}
                 />
             </TableBody>
           </Table>
