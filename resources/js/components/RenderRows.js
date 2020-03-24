de3cb9acd679917779e7654dc7fb18cbadbe6258
nhsykym <nhsykym@gmail.com>
2020-03-24 05:23:25 +0000

    #38 Change 削除ボタンを編集ページの中に移動

--- resources/js/components/RenderRows.js
+++ resources/js/components/RenderRows.js
@@ -6,29 +6,14 @@ import axios from 'axios';
 
 
 const RenderRows = (props) => {
-    const handleDelete = (e) => {
-        axios
-            .post('/api/delete', {
-                id: e.target.name,
-                headers: { 'Authorization': 'Bearer ' + props.token }
-            })
-            .then(res => {
-                props.updateTable(res.data);
-                console.log('deleted');
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    };
-
     return props.expenses.map(expense => {
         return (
             <TableRow key={expense.id}>
                 <TableCell>{expense.purchased_at}</TableCell>
                 <TableCell>{expense.title}</TableCell>
                 <TableCell>{expense.money}</TableCell>
+                <TableCell>{expense.categoryName}</TableCell>
                 <TableCell><button className="btn btn-primary"><Link style={styles.Link} to={'/edit/' + expense.id}>編集</Link></button></TableCell>
-                <TableCell><button className="btn btn-danger" name={expense.id} onClick={handleDelete}>削除</button></TableCell>
             </TableRow>
         );
     });
@@ -39,6 +24,5 @@ const styles = {
         color: "#fff",
     }
 };
-
 
 export default withRouter(RenderRows);
