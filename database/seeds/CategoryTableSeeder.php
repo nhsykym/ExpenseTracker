901b63da6f3506ab4520533f2488fc6517418c53
nhsykym <nhsykym@gmail.com>
2020-03-25 03:39:12 +0000

    #49 Add  カテゴリを追加

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -11,11 +11,15 @@ class CategoryTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('categories')->insert([
-            'name' => 'Book'
-        ]);
-        DB::table('categories')->insert([
-            'name' => 'Vegitable'
-        ]);
+        $categories = [
+            ['name' => '食費'],
+            ['name' => '日用品費'],
+            ['name' => '交際・娯楽費'],
+            ['name' => '住居費'],
+            ['name' => '水道光熱費'],
+            ['name' => '通信費'],
+            ['name' => 'その他']
+        ];
+        DB::table('categories')->insert($categories);
     }
 }
